962e25a3d39d866bd80372315cfd5933b27f8558
Viktor Vartdal Johansen <viktor.v.johansen@hotmail.com>
2019-05-03 17:13:40 +0200

    navbar fixed top in both navbar and profile

--- tweet-service/src/main/resources/data.sql
+++ tweet-service/src/main/resources/data.sql
@@ -1,3 +1,1 @@
-INSERT INTO tweet (id_parent, id_user, text, timestamp, url_image)
-VALUES
-('null', 1, 'test', TIMESTAMP('2009-03-29','04:07:54'), 'null');
+INSERT INTO tweet (id_parent, id_user, text, timestamp, url_image) VALUES ('null', 1, 'test', TIMESTAMP('2009-03-29','04:07:54'), 'null');
